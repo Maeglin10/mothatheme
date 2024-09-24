9e652f1a391b0c241a46374c052837d67f416c65
Maeglin10 <v.milliand@gmail.com>
2024-09-24 14:53:12 +0200

    Mise en place thème + duplication page.php et single.php

---
+++ footer.php
@@ -0,0 +1,16 @@
+<footer class="site-footer">
+    <div class="container">
+        <nav class="footer-navigation">
+            <ul>
+            <li><a href="/a-propos">À propos</a></li>
+<li><a href="/mentions-legales">Mentions légales</a></li>
+
+            </ul>
+        </nav>
+        <p class="copyright">Tous droits réservés &copy; <?php echo date('Y'); ?></p>
+    </div>
+</footer>
+
+<?php wp_footer(); ?>
+</body>
+</html>
