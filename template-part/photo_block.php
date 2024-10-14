ab06c1f62fafee9e8ea5f1f7fd3024055f4aed1d
Maeglin10 <v.milliand@gmail.com>
2024-10-14 13:16:36 +0200

    mise en place icone oeil + categorie sur l'overlay

--- template-part/photo_block.php
+++ template-part/photo_block.php
@@ -8,42 +8,50 @@
                 'posts_per_page' => 8,           // Nombre de publications à afficher par page
                 'orderby' => 'date',             // Trier par date
                 'order' => 'DESC',               // Ordre descendant
-            );        
+            );
 
             $custom_posts_query = new WP_Query($args_custom_posts);
 
             // Boucle pour afficher les publications
-            if ($custom_posts_query->have_posts()) :
-                while ($custom_posts_query->have_posts()) :
+            if ($custom_posts_query->have_posts()):
+                while ($custom_posts_query->have_posts()):
                     $custom_posts_query->the_post();
-            ?>
-            <div class="custom-post-thumbnail">
-                <a href="<?php the_permalink(); ?>">
-                    <?php if (has_post_thumbnail()) : ?>
-                        <div class="thumbnail-wrapper">
-                            <?php the_post_thumbnail('large'); ?>
-                            <div class="thumbnail-overlay">
-                                <div class="overlay-content">
-                                    <p class="photo-reference"><?php echo esc_html(get_field('reference_photo')); ?></p>
-                                    <p class="photo-category">
-                                        <?php
-                                        $related_categories = get_the_terms(get_the_ID(), 'categorie');
-                                        if ($related_categories) {
-                                            $category_names = array_map(function($category) {
-                                                return esc_html($category->name);
-                                            }, $related_categories);
-                                            echo implode(', ', $category_names);
-                                        }
-                                        ?>
-                                    </p>
+                    ?>
+                    <div class="custom-post-thumbnail">
+                        <a href="<?php the_permalink(); ?>">
+                            <?php if (has_post_thumbnail()): ?>
+                                <div class="thumbnail-wrapper">
+                                    <?php the_post_thumbnail('large'); ?>
+                                    <div class="thumbnail-overlay">
+                                        <div class="overlay-content">
+                                            <!-- Icône au centre -->
+                                            <div class="icon-center">
+                                                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/eye-icon.png"
+                                                    alt="icone-oeil">
+                                            </div>
+                                            <!-- Référence en bas à gauche -->
+                                            <p class="photo-reference"><?php echo esc_html(get_field('reference_photo')); ?></p>
+                                            <!-- Catégorie en bas à droite -->
+                                            <p class="photo-category">
+                                                <?php
+                                                $related_categories = get_the_terms(get_the_ID(), 'categorie');
+                                                if ($related_categories) {
+                                                    $category_names = array_map(function ($category) {
+                                                        return esc_html($category->name);
+                                                    }, $related_categories);
+                                                    echo implode(', ', $category_names);
+                                                }
+                                                ?>
+                                            </p>
+                                        </div>
+                                    </div>
+
                                 </div>
-                            </div>
-                        </div>
-                    <?php endif; ?>
-                </a>
-            </div>
-            <?php endwhile; ?>
-            <?php wp_reset_postdata(); ?>
+                            <?php endif; ?>
+                        </a>
+                    </div>
+                <?php endwhile; ?>
+                <?php wp_reset_postdata(); ?>
             <?php else: ?>
                 <p>Aucune photo à afficher.</p>
             <?php endif; ?>
@@ -53,4 +61,3 @@
         </div>
     </div>
 </section>
-
